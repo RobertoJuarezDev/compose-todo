99f535d28da1a19a6c7ba3950b1139f41a8219a3
RobertoJuarez <roberto.juarez.dev@gmail.com>
2023-10-23 18:30:01 -0600

    :arrow_up: upgrade compile and target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.ju4r3z.composetodoapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.ju4r3z.composetodoapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
