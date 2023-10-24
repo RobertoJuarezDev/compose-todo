81802390f6d759adff7841eb9a0495b0e898a270
RobertoJuarez <roberto.juarez.dev@gmail.com>
2023-10-23 18:43:37 -0600

    :sparkles: added default text

--- app/src/main/java/com/ju4r3z/composetodoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/ju4r3z/composetodoapp/addtasks/ui/TasksScreen.kt
@@ -1,8 +1,9 @@
 package com.ju4r3z.composetodoapp.addtasks.ui
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
-
+    Text(text = "hello")
 }
